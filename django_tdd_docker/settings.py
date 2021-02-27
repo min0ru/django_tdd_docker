4e27c4aa489005a18e800af84cde36e1d2723841
min0ru <2010min0ru@gmail.com>
2021-02-27 13:48:05 +0300

    Settings formatting.

--- django_tdd_docker/settings.py
+++ django_tdd_docker/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'django_tdd_docker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': []
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
