38743627a77505610e6c6545fd30fb60dab4974d
min0ru <2010min0ru@gmail.com>
2021-03-02 01:10:40 +0300

    Test for new Movie model.

---
+++ tests/movies/test_models.py
@@ -0,0 +1,15 @@
+import pytest
+
+from movies.models import Movie
+
+
+@pytest.mark.django_db
+def test_movie_model():
+    movie = Movie(title='Covid Space Attack 2022', genre='comedy', year='2021')
+    movie.save()
+    assert movie.title == 'Covid Space Attack 2022'
+    assert movie.genre == 'comedy'
+    assert movie.year == '2021'
+    assert movie.created_date
+    assert movie.updated_date
+    assert str(movie) == movie.title
