f64d37a9277a10d889f264e26caeb72d3353b8f3
min0ru <2010min0ru@gmail.com>
2021-03-02 01:11:52 +0300

    Admin page for Movie model.

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Movie
 
 
-admin.site.register(CustomUser, UserAdmin)
+@admin.register(CustomUser)
+class UserAdmin(DefaultUserAdmin):
+    pass
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'genre', 'year', 'created_date', 'updated_date']
+    fields = ['id', 'title', 'genre', 'year', 'created_date', 'updated_date']
+    readonly_fields = ['id', 'created_date', 'updated_date']
