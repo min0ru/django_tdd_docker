07ef894c18026fa7b0e9323fcc41e796ba1330ad
min0ru <2010min0ru@gmail.com>
2021-03-02 01:11:04 +0300

    Movie model implementation.

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,17 @@
-from django.db import models
+from django.db.models import Model, CharField, DateTimeField
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Movie(Model):
+    title = CharField(max_length=255)
+    genre = CharField(max_length=255)
+    year = CharField(max_length=255)
+    created_date = DateTimeField(auto_now_add=True)
+    updated_date = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
