cf9cef5491604c9fed4f98b00cf6767da1d5be48
min0ru <2010min0ru@gmail.com>
2021-03-06 17:16:20 +0300

    Django security settings.

--- django_tdd_docker/settings.py
+++ django_tdd_docker/settings.py
@@ -143,3 +143,17 @@ AUTH_USER_MODEL = "movies.CustomUser"
 # Disable Browsable API for production
 if not DEBUG:
     REST_FRAMEWORK = {"DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)}
+
+# Security settings
+if not DEBUG:
+   SECURE_HSTS_SECONDS = 3600
+   SECURE_CONTENT_TYPE_NOSNIFF = True
+   SECURE_BROWSER_XSS_FILTER = True
+   SECURE_SSL_REDIRECT = True
+   SESSION_COOKIE_SECURE = True
+   CSRF_COOKIE_SECURE = True
+   X_FRAME_OPTIONS = "DENY"
+   SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+   SECURE_HSTS_PRELOAD = True
+   SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+   SECURE_REFERRER_POLICY = "same-origin"
